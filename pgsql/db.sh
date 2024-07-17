ee40d3d2e502a43c55e380b2ffaa169a70cb32c7
manupanand <manupanandindianinvestor@gmail.com>
2024-07-17 21:00:31 +0530

    new commands privilege

--- pgsql/db.sh
+++ pgsql/db.sh
@@ -15,3 +15,11 @@ DROP DATABASE databasename;
 CREATE USER newusername WITH PASSWORD 'password';
 #giving all privileges to user-connect,create,temp etc.
 GRANT ALL PRIVILEGES ON DATABASE databasename TO username;
+#giving connect privileges
+GRANT CONNECT ON DATABASE databasename TO username;
+#giving create privileges
+GRANT CREATE ON DATABASE database TO username;
+#usage privilege on schema
+GRANT USAGE ON SCHEMA schemaname TO username;
+#grant privilege on table
+GRANT SELECT ON TABLE usertable TO username;
