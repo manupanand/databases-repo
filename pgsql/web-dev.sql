352a5b513f3d1cebe3ddb0cd8dfd8b7107f78b24
manupanand <manupanandindianinvestor@gmail.com>
2024-07-21 00:16:09 +0530

    select - web dev

---
+++ pgsql/web-dev.sql
@@ -0,0 +1,7 @@
+-- comment -sql for web development
+--select  / retrieve some data from  user table 
+SELECT * from user;
+--select recieves data from one or more tables.
+-- select statement do not alter the state of the database.
+SELECT id from users;--select a single field/ 
+SELECT id,name from users;-- select multiple fields
